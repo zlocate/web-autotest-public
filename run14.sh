d2c03c79d5dacb1857ac6e26cd43f8604b840c66
KulikAlex <KulikAlex@github.com>
2021-11-16 14:30:02 +0000

    a21c2f9f9f6fc996628d297b335d038442626047

---
+++ run14.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function print_red(){
+	echo -e "\e[31m$@\e[0m"
+}
+
+function check(){
+	if [[ $? != 0 ]]
+		then
+		print_red "FAILED"
+		exit 1
+	fi
+}
+
+node web-autotest-public/proj14.js
+check
