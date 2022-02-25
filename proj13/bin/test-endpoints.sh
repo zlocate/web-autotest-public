bdd7884a9a8d6007cc85e77348b1b1afff58b3f7
rpill <rpill@github.com>
2022-02-25 11:24:29 +0000

    d0fbf3c8976d9924de7b57f178150dfc4ad2bca1

--- proj13/bin/test-endpoints.sh
+++ proj13/bin/test-endpoints.sh
@@ -13,5 +13,5 @@ function check(){
 }
 
 npm install -g newman
-newman run https://www.getpostman.com/collections/90361e4f436c6d5bf7be --color on
+newman run https://www.getpostman.com/collections/ebc7f98625878907b2c3 --color on
 check
