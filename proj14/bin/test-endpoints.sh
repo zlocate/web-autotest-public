d8fad5d252288e4ee9c4d49873712d2484acc939
rpill <rpill@github.com>
2022-02-25 11:57:25 +0000

    0261aa6c96ed1892b79d4e10ecf4d724c19dd931

--- proj14/bin/test-endpoints.sh
+++ proj14/bin/test-endpoints.sh
@@ -16,5 +16,5 @@ echo 'до 14.03 сдается по старому флоу'
 exit 0
 
 npm install -g newman
-newman run https://www.getpostman.com/collections/ac51bed867a2db09ac1a --color on
+newman run https://www.getpostman.com/collections/155d834872acb2acab87 --color on
 check
