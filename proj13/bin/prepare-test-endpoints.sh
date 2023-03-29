ec6cf56a706e42e52749eb196f6ac3777e030aa2
zlocate <zlocate@github.com>
2023-03-29 00:05:00 +0000

    2e53f30fa977409e841fbb0341b7358382b4f333

--- proj13/bin/prepare-test-endpoints.sh
+++ proj13/bin/prepare-test-endpoints.sh
@@ -2,7 +2,7 @@
 
 source web-autotest-public/scripts.sh
 
-npm install -g pnpm
-pnpm i --frozen-lockfile --prefix web-autotest-public/
+npm install -g pnpm@7.30.3
+pnpm i --fix-lockfile --prefix web-autotest-public/
 node web-autotest-public/proj13/prepare-test-endpoints.js
 check $?
--- proj13/bin/test-config.sh
+++ proj13/bin/test-config.sh
@@ -3,7 +3,7 @@
 source web-autotest-public/scripts.sh
 
 npm i -f
-npm install -g pnpm
-pnpm i --frozen-lockfile --prefix web-autotest-public/
+npm install -g pnpm@7.30.3
+pnpm i --fix-lockfile --prefix web-autotest-public/
 node web-autotest-public/proj13/test-config.js
 check $?
