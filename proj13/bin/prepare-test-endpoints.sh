360ae2405c30d9c534b9d1bf272563292f55acc0
Denis <zlocated@gmail.com>
2023-11-09 15:47:14 +0300

    Update prepare-test-endpoints.sh

--- proj13/bin/prepare-test-endpoints.sh
+++ proj13/bin/prepare-test-endpoints.sh
@@ -3,6 +3,11 @@
 source web-autotest-public/scripts.sh
 
 npm install -g pnpm@7.30.3
+echo "Понижаем версию mongoose"
+npm explain mongoose|| true
+npm i mongoose@6 || true
+npm explain mongoose|| true
+echo "Закончили понижение версии mongoose"
 pnpm i --fix-lockfile --prefix web-autotest-public/
 node web-autotest-public/proj13/prepare-test-endpoints.js
 check $?
